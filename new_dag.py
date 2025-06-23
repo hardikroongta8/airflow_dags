2b5d1e56b29d71f8e354dd9fd3a671ba65c6e5cd
Hardik Roongta <hardikroongta8@gmail.com>
2025-06-23 16:25:39 +0530

    initial commit

---
+++ new_dag.py
@@ -0,0 +1,11 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    "example_1",
+    start_date=datetime(2025, 1, 1),
+    schedule="@daily", catchup=False
+) as dag:
+    hello = BashOperator(task_id="task", bash_command="whoami")
+    print(hello)
